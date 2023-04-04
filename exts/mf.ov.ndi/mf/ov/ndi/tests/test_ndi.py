005186dc67c271a52511e8d41f6671ac0677789b
Frédéric Lestage <fredericl@momentfactory.com>
2023-04-04 15:39:50 -0400

    Added test files

---
+++ exts/mf.ov.ndi/mf/ov/ndi/tests/test_ndi.py
@@ -0,0 +1,17 @@
+import omni.kit.test
+import omni.kit.ui_test as ui_test
+import mf.ov.ndi
+
+
+class Test(omni.kit.test.AsyncTestCase):
+    # async def setUpClass(self):
+    #    pass
+
+    # async def tearDownClass(self):
+    #    pass
+
+    async def setUp(self):
+        pass
+
+    async def tearDown(self):
+        pass
