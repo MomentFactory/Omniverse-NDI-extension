d2175a5b942d5278848d0c80d15a185181b6121b
Frédéric Lestage <fredericl@momentfactory.com>
2023-07-19 13:50:50 -0400

    hotfix NDIlib dependency

--- exts/mf.ov.ndi/mf/ov/ndi/deps/NDIlib/__init__.py
+++ exts/mf.ov.ndi/mf/ov/ndi/deps/NDIlib/__init__.py
@@ -4,4 +4,4 @@ import sys
 if os.name == 'nt' and sys.version_info.major >= 3 and sys.version_info.minor >= 8:
     os.add_dll_directory(os.path.dirname(__file__))
 
-from NDIlib import *
+from .NDIlib import *
